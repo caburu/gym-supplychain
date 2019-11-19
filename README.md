ca7218b874b4ba87ec674c7ca0432c973ec67efa
Julio Cesar Alves <julio.caburu@gmail.com>
2019-11-19 16:26:52 -0300

    Uso do Cliffwalk como ambiente apenas para teste.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gym-supplychain
-An OpenAI Gym environment for Supply Chain problems
+
+An OpenAI Gym environment for Supply Chain problems.
+
+## Installation
+
+Install the package with `pip install -e gym-supplychain` and create create an instance of the environment with `gym.make('gym_supplychain:supplychain-v0')`.
--- gym_supplychain/envs/supplychain_env.py
+++ gym_supplychain/envs/supplychain_env.py
@@ -2,16 +2,62 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-class SupplyChainEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
+from copy import deepcopy # retirar (é do Cliffwalk)
+import numpy as np
 
-  def __init__(self):
-    ...
-  def step(self, action):
-    ...
-  def reset(self):
-    ...
-  def render(self, mode='human'):
-    ...
-  def close(self):
-    ...
+class SupplyChainEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def observation(self, state): # retirar (é do Cliffwalk)
+        return state[0] * self.cols + state[1]
+
+    def __init__(self):
+        self.rows = 4
+        self.cols = 12
+        self.start = [0,0]
+        self.goal = [0,11]
+        self.current_state = None
+
+        # São 4 ações: cima, baixo, esquerda e direita
+        self.action_space = spaces.Discrete(4)
+
+         # observation is the x, y coordinate of the grid
+        self.observation_space = spaces.Discrete(self.rows*self.cols-1)
+
+
+    def step(self, action):
+        new_state = deepcopy(self.current_state)
+
+        if action == 0: #right
+            new_state[1] = min(new_state[1]+1, self.cols-1)
+        elif action == 1: #down
+            new_state[0] = max(new_state[0]-1, 0)
+        elif action == 2: #left
+            new_state[1] = max(new_state[1]-1, 0)
+        elif action == 3: #up
+            new_state[0] = min(new_state[0]+1, self.rows-1)
+        else:
+            raise Exception("Invalid action.")
+        self.current_state = new_state
+
+        reward = -1.0
+        is_terminal = False
+        if self.current_state[0] == 0 and self.current_state[1] > 0:
+            if self.current_state[1] < self.cols - 1:
+                reward = -100.0
+                self.current_state = deepcopy(self.start)
+            else:
+                is_terminal = True
+
+        return self.observation(self.current_state), reward, is_terminal, {}
+
+    def reset(self):
+        self.current_state = self.start  # An empty NumPy array
+
+        return self.observation(self.current_state)
+
+    def render(self, mode='human'):
+        pass
+
+    def close(self):
+        pass
