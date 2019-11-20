ffb0f360f24368baaff60c80f6f14fc9dca453b5
caburu <julio.caburu@gmail.com>
2019-11-20 15:31:43 -0300

    Retirada do código do cliffwalking e início do código de supply chain

--- gym_supplychain/envs/supplychain_env.py
+++ gym_supplychain/envs/supplychain_env.py
@@ -1,59 +1,73 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
-
-from copy import deepcopy # retirar (é do Cliffwalk)
+from copy import deepcopy
 import numpy as np
 
 class SupplyChainEnv(gym.Env):
-    metadata = {'render.modes': ['human']}
+    #metadata = {'render.modes': ['human']}
 
-    def observation(self, state): # retirar (é do Cliffwalk)
-        return state[0] * self.cols + state[1]
+    def code_array(self, array, num_codes):
+        code = 0
+        for i in range(len(array)):
+            code += array[i]*(num_codes**i)
+        return code
 
-    def __init__(self):
-        self.rows = 4
-        self.cols = 12
-        self.start = [0,0]
-        self.goal = [0,11]
+    def decode_array(self, code, num_levels, num_codes):
+        array = [0]*num_levels
+        for i in range(num_levels-1,0,-1):
+            array[i] = code // num_codes**i
+            code = code % num_codes**i
+        array[0] = code
+        return array
+
+    def observation(self, state):
+        return self.code_array(state, self.state_codes)
+
+    def decode_actions(self, action):
+        return self.decode_array(action, self.levels, self.action_codes)
+
+    def __init__(self, env_init_info={}):
+        '''Initial inventory is a list with the initial inventory position for each level
+        '''
+        # Número de níveis da cadeia
+        self.levels = len(env_init_info['initial_inventory'])
+        self.state_codes = 9
+        self.action_codes = 4
+
+        self.inv_cost = 1
+
+        self.start_state = np.asarray(env_init_info['initial_inventory'])
         self.current_state = None
 
-        # São 4 ações: cima, baixo, esquerda e direita
-        self.action_space = spaces.Discrete(4)
+        # Ver uso do MultiDiscrete
 
-         # observation is the x, y coordinate of the grid
-        self.observation_space = spaces.Discrete(self.rows*self.cols)
+        self.action_space = spaces.Discrete(self.action_codes**self.levels)
+        self.observation_space = spaces.Discrete(self.state_codes**self.levels)
 
 
     def step(self, action):
+        actions = np.asarray(self.decode_actions(action))
+
         new_state = deepcopy(self.current_state)
 
-        if action == 0: #right
-            new_state[1] = min(new_state[1]+1, self.cols-1)
-        elif action == 1: #down
-            new_state[0] = max(new_state[0]-1, 0)
-        elif action == 2: #left
-            new_state[1] = max(new_state[1]-1, 0)
-        elif action == 3: #up
-            new_state[0] = min(new_state[0]+1, self.rows-1)
-        else:
-            raise Exception("Invalid action.")
-        self.current_state = new_state
+        self.current_state = new_state + actions
 
-        reward = -1.0
+        for i in range(len(self.current_state)):
+            self.current_state[i] = min(self.state_codes-1, self.current_state[i])
+
+        reward = np.sum(self.inv_cost*self.current_state)
+        if sum(actions == 0): # penaliza ação com tudo zero
+            reward -= 10
+
         is_terminal = False
-        if self.current_state[0] == 0 and self.current_state[1] > 0:
-            if self.current_state[1] < self.cols - 1:
-                reward = -100.0
-                self.current_state = deepcopy(self.start)
-            else:
-                is_terminal = True
+        if np.max(self.current_state) == self.state_codes-1:
+            is_terminal = True
 
         return self.observation(self.current_state), reward, is_terminal, {}
 
     def reset(self):
-        self.current_state = self.start  # An empty NumPy array
-
+        self.current_state = self.start_state
         return self.observation(self.current_state)
 
     def render(self, mode='human'):
