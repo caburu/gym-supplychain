fa07c09ef3a87f49f1362fce3307109ae9a371cd
caburu <julio.caburu@gmail.com>
2019-11-20 10:28:00 -0300

    Retirado e-mail.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='gym_supplychain',
 
       # metadata to display on PyPI
       author="Júlio César Alves",
-      author_email="julio.caburu@gmail,com",
       description="An OpenAI Gym environment for Supply Chain problems",
       keywords="openai gym supply chain environment",
       url="https://github.com/caburu/gym-supplychain"
