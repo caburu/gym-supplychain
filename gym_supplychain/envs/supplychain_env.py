3530ffdcf70eb6a463894a2f056ed1b8334a9909
Julio Cesar Alves <julio.caburu@gmail.com>
2019-11-21 09:10:14 -0300

    Reformulação da cadeia do MIT beer game. Commit de backup

--- gym_supplychain/envs/supplychain_env.py
+++ gym_supplychain/envs/supplychain_env.py
@@ -56,89 +56,136 @@ class SupplyChainEnv(gym.Env):
 
         # Número de níveis da cadeia
         self.levels = 4
+        # A CODIFICAÇÃO DE ESTADOS E AÇÕES DEVERIA ESTAR NO AGENTE E NÃO NO AMBIENTE
         self.state_codes = 9
         self.action_codes = 4
         self.inv_cost = 1
         self.backlog_cost = 2
-        self.initial_ditr = 12 + np.zeros(self.levels+1, dtype=int)
 
-        self.action_space = spaces.Discrete(self.action_codes**self.levels)
-        self.observation_space = spaces.Discrete(self.state_codes**self.levels)
+        # Nos vetores, a posição zero é do retailer
 
         # Quantidade de itens em cada estoque
-        self.initial_inventory = np.asarray(env_init_info['initial_inventory'])
-        self.customer_dem = env_init_info['customer_dem']
-        #self.lead_times = env_init_info['lead_times']
-        self.max_n = len(self.customer_dem)
-        print('max_n', self.max_n)
+
+        # Demanda dos clientes a cada semana
+        self.customer_demand = np.asarray(env_init_info['customer_demand'])
+        # Quantidade inicial de estoque em cada nível
+        self.initial_inventory = 12 + np.zeros(self.levels)
+        # Número de semanas a simular
+        self.max_weeks = len(self.customer_demand)
+        # Máximo leadtime de entrega   ## MUDAR PARA O CASO DO ARTIGO
+        self.max_ship_delay = 2
+        # Valor inicial de ítens em transporte
+        self.initial_shipment_values = 4
+        # Por quanto tempo essas unidades em transporte vão chegar
+        self.initial_shipment_times = 2
+        # Pedidos colocados inicialmente
+        self.initial_orders_value = 4
+
+
+        # Estrutura para guardar todas as entregas. Por tempo, por nível.
+        self.initial_shipment = np.zeros((self.max_weeks+1+self.max_ship_delay, self.levels), dtype=int)
+        # Tratando as entregas pendentes já no momento inicial
+        self.initial_shipment[1:1+self.initial_shipment_times][:] = self.initial_shipment_values
+
+        # Pedidos colocados para o nível acima
+        self.initial_orders_placed = self.initial_orders_value + np.zeros(self.levels, dtype=int)
+        # Pedidos pendentes que chegaram do nível abaixo (posição zero é a demanda do cliente,
+        # e valor colocado aqui é ignorado depois)
+        self.initial_incoming_orders = self.initial_orders_value + np.zeros(self.levels, dtype=int)
 
         self.current_state = None
-        # Ver uso do MultiDiscrete
+
+        # Tratando variáveis do OpenAI Gym environment (# Ver uso do MultiDiscrete)
+        self.action_space = spaces.Discrete(self.action_codes**self.levels)
+        self.observation_space = spaces.Discrete(self.state_codes**self.levels)
 
 
     def step(self, action):
         Y_actions = np.asarray(self.decode_actions(action))
         #Y_actions = np.asarray(self.decode_actions(0))
 
-        # o último nível recebe a demanda do cliente
-        self.A_orders[-1] = self.customer_dem[self.n]
+        self.week += 1
 
-        # 1. Chegada de pedidos encomendados (de acordo com o leadtime)
-        #print('ord', self.A_orders)
-        #print('inv', self.S_inventory)
-        #print('dis', self.T_distr)
-        #print()
+        # 1. Receive inventory and advance shipment delay
 
-        self.S_inventory += self.T_distr[:-1]
+        # Os estoques recebem o que estava previsto nas entregas
+        self.inventory += self.shipments[self.week][:]
+        # Não é necessário "avançar" as entregas, pois estamos guardando todas
+        # no vetor (por tempo)
+
+        # 2. Fill orders
+
+        # Retailer obtém a demanda do cliente
+        self.incoming_orders[0] = self.customer_demand[self.week-1]
+
+        # Valores negativos do estoque indicam o backlog.
+        # logo, tratamos aqui as entregas que chegam (e o backlog está indiretamente
+        # tratado no recebimento do passo anterior e aqui)
+
+        # Faz entrega
+        self.shipments[self.week+self.max_ship_delay][:-1] =  \
+            np.maximum(np.zeros(self.levels-1, dtype=int),
+                       np.minimum(self.inventory[1:], self.incoming_orders[1:]))
+        # Desconta do estoque
+        self.inventory -= self.incoming_orders
 
 
-        # 2. Recebimento de novos pedidos
+        # 3. Record the invetory or backlog
 
+        # Já feito :)
 
-        # 3. Atendimento dos novos pedidos recebidos
+        # 4. Advance the order slips
 
-        self.T_distr[0] = self.A_orders[0]
-        # A quantidade de itens disponíveis são os valores positivos do estoque
-        H_inv_onhand = np.clip(self.S_inventory,0,None)
-        self.T_distr[1:] = np.minimum(H_inv_onhand, self.A_orders[1:])
+        # Movendo os pedidos colocados para os pedidos chegando
+        # Obs: a informação do cliente não é atualizada aqui (valor fica lixo
+        #        até próximo passo)
+        self.incoming_orders[1:] = self.orders_placed[:-1]
 
-        self.S_inventory -= self.A_orders[1:]
+        # Pedidos para a fábrica (último nível) são colocados para entrega
+        self.shipments[self.week+self.max_ship_delay][-1] = self.incoming_orders[-1]
 
-
-        # 4. Decisão de novos pedidos
+        # 5. Place orders
 
         # cada nível passa para o nível acima um pedido de tamanho X+Y
         # onde X é o que recebeu de demanda e Y é a quantidade a decidir pelo agente
-        for i in range(self.levels):
-            self.A_orders[i] = self.A_orders[i+1] + Y_actions[i]
 
-        self.current_state = self.code_state(self.S_inventory)
+        self.orders_placed = self.incoming_orders + Y_actions
+
+        # 6. Tratando agora as questões de Aprendizado (recompensa e próximo estado)
+
+        self.current_state = self.code_state(self.inventory)
 
         # A quantidade de itens disponíveis são os valores positivos do estoque
-        H_inv_onhand = np.clip(self.S_inventory,0,None)
+        inventory_onhand = np.clip(self.inventory,0,None)
         # O backlog é a quantidade de itens que falta no estoque (valores negativos)
-        C_backlog = -np.clip(self.S_inventory,None,0)
+        backlog = -np.clip(self.inventory,None,0)
         # A recompensa é a soma ponderada dos custos de estoque e backlog
-        reward = -np.sum(self.inv_cost*H_inv_onhand + self.backlog_cost*C_backlog)
+        reward = -np.sum(self.inv_cost*inventory_onhand + self.backlog_cost*backlog)
 
-        self.n += 1
-        is_terminal = self.n == self.max_n
+        is_terminal = self.week == self.max_weeks
 
         return self.observation(self.current_state), reward, is_terminal, {}
 
     def reset(self):
-        self.n = 0
-        self.S_inventory = np.copy(self.initial_inventory)
+        self.week = 0
+        self.inventory = np.copy(self.initial_inventory)
+        self.orders_placed = np.copy(self.initial_orders_placed)
+        self.incoming_orders = np.copy(self.initial_incoming_orders)
+        self.shipments = np.copy(self.initial_shipment)
 
-        self.A_orders = np.zeros(self.levels+1, dtype=int)
-
-        self.T_distr = np.copy(self.initial_ditr)
-
-        self.current_state = self.code_state(self.S_inventory)
+        self.current_state = self.code_state(self.inventory)
 
         return self.observation(self.current_state)
 
     def render(self, mode='human'):
+        print('\n' + '='*20)
+        print('Week:\t', self.week)
+        print('Inventory:\t', self.inventory)
+        print('Incoming orders:\t', '[?]', self.incoming_orders[1:])
+        print('Orders placed:\t', self.orders_placed)
+        if self.week < self.max_weeks:
+            print('Next customer demanda:\t', self.customer_demand[self.week])
+        print('Next shipments:\t', self.shipments[self.week+self.max_ship_delay][:])
         pass
 
     def close(self):
